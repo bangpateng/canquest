bf9774c2d01cebb8f25f333a1c3e908b01f1e0d9
Bang Pateng <coinnyari@gmail.com>
2026-06-08 23:56:50 +0700

    hide live campaigns section from landing page

--- apps/web/components/landing/featured-quests-section.tsx
+++ apps/web/components/landing/featured-quests-section.tsx
@@ -1,7 +1,3 @@
-import { fetchFeaturedQuests } from "@/lib/server/featured-quests";
-import { FeaturedCampaigns } from "@/components/landing/featured-campaigns";
-
 export async function FeaturedQuestsSection() {
-  const quests = await fetchFeaturedQuests(20);
-  return <FeaturedCampaigns quests={quests} />;
+  return null;
 }
