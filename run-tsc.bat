8869468033850953c62f079298cd7f2140143fde
Bang Pateng <coinnyari@gmail.com>
2026-06-15 12:24:01 +0700

    feat:earn++==

---
+++ run-tsc.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "c:\Users\Bang Pateng\Documents\can\apps\web"
+node_modules\.bin\tsc.cmd --noEmit 2>&1
---
+++ run-tsc.ps1
@@ -0,0 +1,2 @@
+Set-Location "c:\Users\Bang Pateng\Documents\can\apps\web"
+& "c:\Users\Bang Pateng\Documents\can\apps\web\node_modules\.bin\tsc" --noEmit 2>&1
